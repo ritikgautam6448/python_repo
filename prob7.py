ab80f880f5063249a2465fafa9e9b4e67029760b
Ritik kumar gautam <ritikgautam6448@gmail.com>
2025-08-17 18:35:59 +0530

    save this file prob7.py

---
+++ prob7.py
@@ -0,0 +1,6 @@
+# this question is from the book "Python Programming: An Introduction to Computer Science" by John Zelle
+def sleeping (weekday,vacation):
+    if not weekday or vacation:
+        return True
+    else:
+        return False
