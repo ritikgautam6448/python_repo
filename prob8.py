40c0e8d853329801bf6216294343dc5063bbeb0c
Ritik kumar gautam <ritikgautam6448@gmail.com>
2025-08-17 17:28:17 +0530

    Add comments and execute functions Prob8

--- prob8.py
+++ prob8.py
@@ -1,6 +1,11 @@
-def monkey trouble(a_smile, b_smile):
-if a_smile and b_smile:
-    return True
-if not a_smile and not b_smile:
-    return True
-return False    
+# This is a comment
+
+def monkey_trouble(a_smile, b_smile):
+    if a_smile and b_smile:
+        return True
+    if not a_smile and not b_smile:
+        return True
+    return False
+
+print(monkey_trouble(True, True))   # Expected: True
+print(monkey_trouble(True, False))
