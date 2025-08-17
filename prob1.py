d9cfe1c664ba3f43b71aa4037e4e8cb2d9e68961
Ritik kumar gautam <ritikgautam6448@gmail.com>
2025-08-17 17:36:31 +0530

    change

---
+++ prob1.py
@@ -0,0 +1,9 @@
+def front_back(s):
+    if len(s) <= 1:
+        return s
+    
+    mid = s[1:len(s)-1]   # middle part (excluding first and last char)
+    return s[-1] + mid + s[0]  # last + middle + first
+
+
+print(front_back("code"))  # Expected: eodc
