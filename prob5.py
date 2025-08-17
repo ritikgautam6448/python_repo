27c71c418a528634d0d9d700462ead25680309ee
Ritik kumar gautam <ritikgautam6448@gmail.com>
2025-08-17 18:27:41 +0530

    upload this file prob5.py

---
+++ prob5.py
@@ -0,0 +1,6 @@
+# this function takes an integer n and returns the absolute difference between n and 21.
+def sleeping (weekday,vacation):
+    if not weekday or vacation:
+        return True
+    else:
+        return False
