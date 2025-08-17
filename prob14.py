429821d1cbe0bc5f65c3d9b472337807bc7bef5d
Ritik kumar gautam <ritikgautam6448@gmail.com>
2025-08-17 19:17:59 +0530

    insert file prob14.py

---
+++ prob14.py
@@ -0,0 +1,7 @@
+# this function checks if a person is sleeping based on the day of the week and vacation status
+# weekday is True if it is a weekday, False if it is a weekend
+def sleeping (weekday,vacation):
+    if not weekday or vacation:
+        return True
+    else:
+        return False
