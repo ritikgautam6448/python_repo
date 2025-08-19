0926d870a2784245ad04a4a69e427f77c223973a
Ritik kumar gautam <ritikgautam6448@gmail.com>
2025-08-19 11:50:54 +0530

    change this file for dev child

--- prob16.py
+++ prob16.py
@@ -2,12 +2,19 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    return "home"
+@app.route("/get-user/<user_id>")
+def get_user(user_id):
+    user_data = {
+        "user_id": user_id,
+        "name": "John Doe",
+        "email": "ritikgautam6448@gmail.com"
+    }
+     
+    extra = request.args.get("extra")
+    if extra:
+        user_data["extra"] = extra
 
-
-
+    return jsonify(user_data), 100
     if __name__ == '__main__':
         app.run(debug=True)
-        
+     
