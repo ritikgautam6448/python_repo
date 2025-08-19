350dc1745deb950ea86cbc51ebec5b9e9f782ab6
Ritik kumar gautam <ritikgautam6448@gmail.com>
2025-08-19 12:04:09 +0530

    insert this file

--- prob16.py
+++ prob16.py
@@ -15,6 +15,16 @@ def get_user(user_id):
         user_data["extra"] = extra
 
     return jsonify(user_data), 100
+
+    @app.route("/get-user", methods=["POST"])
+    def create_user():
+        data = request.get_json()
+
+        return jsonify(data), 201
+        
+            
+
+    
     if __name__ == '__main__':
         app.run(debug=True)
      
